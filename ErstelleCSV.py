fc354d6b524d05b8cdb4b8c60d5a025d8ef9b65c
Mechtilde <ooo@mechtilde.de>
2013-08-22 20:14:57 +0200

    Zweite Version Nachbearbeitung durc schezy

--- ErstelleCSV.py
+++ ErstelleCSV.py
@@ -3,7 +3,8 @@
 #
 #  ErstelleCSV.py
 #  
-#  Copyright 2013 Mechtilde Stehmann <mechtilde@stephan>
+#  Copyright 2013 Mechtilde Stehmann <ooo@mechtilde.de>
+#  Copyright 2013 Johannes Spielmann <jps@shezi.de>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -22,6 +23,48 @@
 
 from tkinter import filedialog
 from tkinter import *
+
+
+class LineReplacer(object):
+    """Replaces substrings in a list of strings."""
+
+    def __init__(self, lines=None, replacements=None):
+        
+        if lines is None:
+            lines = []
+        if replacements is None:
+            replacements = (
+                ("‖", "\t\""),
+                ("\t20", "\t\"\t20"),
+                ("\t\t", "\t"),
+                ("\t\t\"\t20", "\"\t20"),
+            )
+        
+        self.lineslist = lines
+        self.replacements = replacements
+
+    def do_replacement(self):
+        """Executes all replacements on all lines and returns the resulting list."""
+
+        for oldstr, newstr in self.replacements:
+            
+            self.replaceStrings(oldstr, newstr)
+            
+        return self.lineslist
+        
+
+    def replaceStrings(self, oldstring, newstring):
+        """Ersetzen der Zeichen"""
+        i = 0 # Zähler für die Zeilen
+        for line in self.lineslist:
+            line = line.replace(oldstring, newstring)
+            self.lineslist[i] = line
+            i = i + 1
+
+        return(self.lineslist)
+
+
+
 
 class ErsetzenCSV(object):
 	"""Ersetzt Strings durch andere und speichert in neuer Datei ab."""
@@ -50,16 +93,6 @@ class ErsetzenCSV(object):
 
 		return(self.lineslist)
 
-	def replaceStrings(self, oldstring, newstring):
-		"""Ersetzen der Zeichen"""
-		i = 0 # Zähler für die Zeilen
-		for line in self.lineslist:
-			line = line.replace(oldstring, newstring)
-			self.lineslist[i] = line
-			i = i + 1
-
-		return(self.lineslist)
-
 	def writeNewFile(self):
 		filesplit = self.filename.split(".")
 		newfilename = filesplit[0]+ "_New" + ".csv"
@@ -83,17 +116,17 @@ class Dialog(object):
 			filename = "XXX"
 		return(filename)
 
-def main():
-	dia = Dialog()
-	filename = dia.fileOpenDialog()
-	if filename != "XXX":
-		escsv = ErsetzenCSV(filename)
-		escsv.replaceStrings("‖","\t\"")
-		escsv.replaceStrings("\t\t","\t")
-		escsv.replaceStrings("\t20","\t\"\t20")
-		escsv.replaceStrings("\t\t\"\t20","\"\t20")
-		escsv.writeNewFile()
-	return 0
+#def main():
+#	dia = Dialog()
+#	filename = dia.fileOpenDialog()
+#	if filename != "XXX":
+#		escsv = ErsetzenCSV(filename)
+#		escsv.replaceStrings("‖","\t\"")
+#		escsv.replaceStrings("\t\t","\t")
+#		escsv.replaceStrings("\t20","\t\"\t20")
+#		escsv.replaceStrings("\t\t\"\t20","\"\t20")
+#		escsv.writeNewFile()
+#	return 0
 
 if __name__ == '__main__':
 	main()
